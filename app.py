f64ace540fba4aeb8f5f89f57849a88afcaf48e8
Anshu Singh <69201747+anshu15183@users.noreply.github.com>
2024-05-26 21:28:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ if st.button('Recommend',):
         st.write(f'<div style="{text_style}">Popularity  : {popularity}</div></br>', unsafe_allow_html=True)
     with col3:
          if preview_url:
-            st.markdown(f'<style>{keyframes}</style>'f'<a href="{preview_url}" target="_blank" style="{link_style}">🎵 Click Here To Play 🎧 Music 🎵</a></br>',unsafe_allow_html=True)
+            st.markdown(f'<style>{keyframes}</style>'f'<a href="{preview_url}" target="_blank" style="{link_style}">🎵 Play Preview 🎵</a></br>',unsafe_allow_html=True)
             st.markdown(f'</br></br><div style="{center_style}">👆</div></br></br>',unsafe_allow_html=True)
     
     logging.info("Successfully show user enter movie with information")
